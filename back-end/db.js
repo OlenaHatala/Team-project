bab6e84e857270dc327e82aae47715b5791cbc54
Soerful <andriy.ishchukk@gmail.com>
2023-02-03 07:19:40 +0200

    Test backend deleted, added .env.example

--- back-end/db.js
+++ back-end/db.js
@@ -1,5 +1,5 @@
 const Mongoose = require("mongoose")
-const localDB = `mongodb+srv://Andriy:2224@clusterqa.dhdgpcl.mongodb.net/tableDB?retryWrites=true&w=majority`
+const localDB = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@clusterqa.dhdgpcl.mongodb.net/tableDB?retryWrites=true&w=majority`
 const connectDB = async () => {
   await Mongoose.connect(localDB, {
     useNewUrlParser: true,
--- back-end/server.js
+++ back-end/server.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require("express")
 const connectDB = require("./db")
 const app = express()
