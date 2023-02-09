2784eb6332aaeb0bbb1c05f2c5af7cb74abfeaaa
Olena Hatala <hatala.or@gmail.com>
2023-02-09 21:49:34 +0200

    task completed

--- back-end/controllers/ticketController.js
+++ back-end/controllers/ticketController.js
@@ -1,28 +1,40 @@
 const Ticket = require('../models/Ticket')
-const User = require('../models/User')
+//const User = require('../models/User')
 const asyncHandler = require('express-async-handler')
 
 
-const createNewTicket = asyncHandler(async (req, res) => {
+const create = asyncHandler(async (req, res) => {
     const { table_id, user_id, datetime, duration, is_outdated, enabled, confirmed } = req.body
 
     if (!table_id || !user_id || !datetime || !duration || !is_outdated || !enabled || !confirmed) {
         return res.status(400).json({ message: 'All fields are required' })
     }
 
-    // Create and store the new user 
-    const note = await Note.create({ user, title, text })
-
-    if (note) { // Created 
-        return res.status(201).json({ message: 'New note created' })
-    } else {
-        return res.status(400).json({ message: 'Invalid note data received' })
-    }
-
+    try {
+        await Ticket.create({
+        table_id,
+        user_id, 
+        datetime, 
+        duration, 
+        is_outdated, 
+        enabled, 
+        confirmed
+        }).then((ticket) =>
+          res.status(200).json({
+            message: "Ticket created successfully",
+            ticket,
+          })
+        )
+      } catch (error) {
+        res.status(400).json({
+          message: "Ticket was not created",
+          error: error.message,
+        })
+      }
 })
 
 module.exports = {
-    createNewTicket,
+    create,
     //updateTicket,
     //deleteTicket
 }
