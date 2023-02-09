65ae57b5359a4eecd8d2f895dc0807520c7ef663
Olena Hatala <hatala.or@gmail.com>
2023-02-09 17:39:48 +0200

    add Ticket.js

--- back-end/models/Ticket.js
+++ back-end/models/Ticket.js
@@ -1,4 +1,5 @@
 const Mongoose = require("mongoose")
+<<<<<<< HEAD
 
 
 const TicketSchema = new Mongoose.Schema({
@@ -13,24 +14,56 @@ const TicketSchema = new Mongoose.Schema({
         datetime: {
             type: Date,
             //min: new Date(),
+=======
+const TicketSchema = new Mongoose.Schema({
+        table_id: {
+            type: ObjectID,
+            default: "",
+            required: true,
+        },
+        user_id: {
+            type: ObjectID,
+            default: "",
+            required: true,
+        },
+        datetime: {
+            type: Date,
+            min: Date.now(),
+>>>>>>> 89ce2bc (add Ticket.js)
             required: true,
         },
         duration: {
             type: Number,
             min: 0,
+<<<<<<< HEAD
             default: 60,
+=======
+            required: true,
+>>>>>>> 89ce2bc (add Ticket.js)
         },
         is_outdated: {
             type: Boolean,
             default: false,
+<<<<<<< HEAD
+=======
+            required: true,
+>>>>>>> 89ce2bc (add Ticket.js)
         },
         enabled: {
             type: Boolean,
             default: false,
+<<<<<<< HEAD
+=======
+            required: true,
+>>>>>>> 89ce2bc (add Ticket.js)
         },
         confirmed: {
             type: Boolean,
             default: false,
+<<<<<<< HEAD
+=======
+            required: true,
+>>>>>>> 89ce2bc (add Ticket.js)
         },
     },
     {versionKey: false}    
