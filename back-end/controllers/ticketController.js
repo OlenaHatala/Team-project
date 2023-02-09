4d6712b3151ba03678d7c6015c3b3e2af876b505
Olena Hatala <hatala.or@gmail.com>
2023-02-09 18:51:50 +0200

    add ticketController.js

---
+++ back-end/controllers/ticketController.js
@@ -0,0 +1,28 @@
+const Ticket = require('../models/Ticket')
+const User = require('../models/User')
+const asyncHandler = require('express-async-handler')
+
+
+const createNewNote = asyncHandler(async (req, res) => {
+    const { table_id, user_id, datetime, duration, is_outdated, enabled, confirmed } = req.body
+
+    if (!table_id || !user_id || !datetime || !duration || !is_outdated || !enabled || !confirmed) {
+        return res.status(400).json({ message: 'All fields are required' })
+    }
+
+    // Create and store the new user 
+    const note = await Note.create({ user, title, text })
+
+    if (note) { // Created 
+        return res.status(201).json({ message: 'New note created' })
+    } else {
+        return res.status(400).json({ message: 'Invalid note data received' })
+    }
+
+})
+
+module.exports = {
+    createNewNote,
+    //updateNote,
+    //deleteNote
+}
