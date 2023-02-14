d7a7f912643f2057f87720f08402441d199f98ce
Olena Hatala <hatala.or@gmail.com>
2023-02-14 14:44:58 +0200

    add deleteBoard function to boardController.js

--- back-end/routes/boardRoutes.js
+++ back-end/routes/boardRoutes.js
@@ -2,8 +2,9 @@ const express = require('express')
 const router = express.Router()
 
 
-const {create} = require('../controllers/boardController')
+const {create, deleteBoard} = require('../controllers/boardController')
 
 router.route("/create").post(create);
+router.route("/deleteBoard").delete(deleteBoard);
 
 module.exports = router
