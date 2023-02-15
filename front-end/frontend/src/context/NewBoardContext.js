0a8a81acbb799672df395a761a0b47511f5b67e8
Soerful <andriy.ishchukk@gmail.com>
2023-02-15 14:36:30 +0200

    new board page

--- front-end/frontend/src/context/NewBoardContext.js
+++ front-end/frontend/src/context/NewBoardContext.js
@@ -63,7 +63,6 @@ const NewBoardContext = createContext({
   setDetails: (details) => {},
   setSettings: (nav, settings) => {},
   setSchedule: () => {},
-  submitForm: () => {},
 });
 
 export const NewBoardProvider = ({ children }) => {
@@ -197,27 +196,6 @@ export const NewBoardProvider = ({ children }) => {
   //   setSun({ disabled, open, close });
   // };
 
-  const submitForm = () => {
-    console.log(details);
-    console.log("settings:");
-    console.log(settings);
-    console.log(`duration: ${duration}`);
-    console.log('mon:');
-    console.log(mon);
-    console.log('tue:');
-    console.log(tue);
-    console.log('wed:');
-    console.log(wed);
-    console.log('thu:');
-    console.log(thu);
-    console.log('fri:');
-    console.log(fri);
-    console.log('sat:');
-    console.log(sat);
-    console.log('sun:');
-    console.log(sun);
-  };
-
   return (
     <NewBoardContext.Provider
       value={{
@@ -246,7 +224,6 @@ export const NewBoardProvider = ({ children }) => {
         setDetails,
         setSettings,
         setDuration,
-        submitForm
       }}
     >
       {children}
--- front-end/frontend/src/pages/AccountRoot.js
+++ front-end/frontend/src/pages/AccountRoot.js
@@ -1,13 +1,11 @@
-import { Outlet } from 'react-router-dom';
-
-import { NewBoardProvider } from '../context/NewBoardContext';
+import { Outlet } from "react-router-dom";
 
 function AccountRootLayout() {
-  console.log('acc-root-layout');
+  console.log("acc-root-layout");
   return (
-    <div className='account-root'>
-    <h1>Profile</h1>
-    <NewBoardProvider><Outlet /></NewBoardProvider>
+    <div className="account-root">
+      <h1>Profile</h1>
+      <Outlet />
     </div>
   );
 }
