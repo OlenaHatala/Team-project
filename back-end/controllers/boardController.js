9b4d20c2301922b73a397b1c907a7299b62be2b5
Olena Hatala <hatala.or@gmail.com>
2023-02-16 12:47:49 +0200

    creation board

--- back-end/controllers/boardController.js
+++ back-end/controllers/boardController.js
@@ -2,19 +2,88 @@ const Board = require('../models/Board')
 const User = require('../models/User')
 const Ticket = require('../models/Ticket')
 const asyncHandler = require('express-async-handler')
-var db = require('../db')
 
 function addMinutes(date, minutes) {
+
     return date.setTime(date.getTime() + minutes*60000);
+}
+const week_index = {
+    'monday': 0,
+    'tuesday': 1,
+    'wednesday': 2,
+    'thursday': 3,
+    'friday': 4,
+    'saturday': 5,
+    'sunday': 6,
 }
 
 const create  = asyncHandler(async (req, res) => {
-    const {owner_id, label, description, service_name, req_confirm, book_num, markup, tickets, address, auto_open} = req.body
+    const {owner_id, label, description, service_name, req_confirm, book_num, markup, address, auto_open} = req.body
+
+    const tickets = [{
+
+        monday:  [],
+        tuesday: [],
+        wednesday: [],
+        thursday: [],
+        friday: [],
+        saturday: [],
+        sunday: []
+    },
+    {
+
+        monday: [],
+        tuesday: [],
+        wednesday: [],
+        thursday: [],
+        friday: [],
+        saturday: [],
+        sunday: []
+    },
+    {
+        
+        monday: [],
+        tuesday: [],
+        wednesday: [],
+        thursday: [],
+        friday: [],
+        saturday: [],
+        sunday: []
+    },
+    {
+        
+        monday: [],
+        tuesday: [],
+        wednesday: [],
+        thursday: [],
+        friday: [],
+        saturday: [],
+        sunday: []
+    },
+    {
+        
+        monday: [],
+        tuesday: [],
+        wednesday: [],
+        thursday: [],
+        friday: [],
+        saturday: [],
+        sunday: []
+    },
+    {
+        
+        monday: [],
+        tuesday: [],
+        wednesday: [],
+        thursday: [],
+        friday: [],
+        saturday: [],
+        sunday: []
+    }]
 
     try {
 
-        
-        // console.log(markup)
+
         const board = await Board.create({
             owner_id,
             label,
@@ -30,7 +99,6 @@ const create  = asyncHandler(async (req, res) => {
         for (i = 0; i<6; i++)
         {
             for (const day in markup.days) {
-            // console.log(markup.days[day])
             const open_hour = markup.days[day].open.split(':')[0];
             const open_min = markup.days[day].open.split(':')[1];
             
@@ -42,10 +110,9 @@ const create  = asyncHandler(async (req, res) => {
             const open_time = new Date(2000, 1, 1)
             open_time.setMinutes(open_min)
             open_time.setHours(open_hour)
-            console.log('1')
             const close_time = new Date(2000, 1, 1)
-            close_time.setHours(open_hour)
-            close_time.setMinutes(open_min)
+            close_time.setHours(close_hour)
+            close_time.setMinutes(close_min)
 
             let ticket_time = open_time.getTime()
             const add_min = (min) => {
@@ -56,47 +123,34 @@ const create  = asyncHandler(async (req, res) => {
                 const current_day = new Date().getDay()
                 let markup_day = 0
                 
-                if (day === 'tuesday')
-                {
-                    markup_day = 1
-                }
-                if (day === 'wednesday')
-                {
-                    markup_day = 2
-                }
-                if (day === 'thursday')
-                {
-                    markup_day = 3
-                }
-                if (day === 'friday')
-                {
-                    markup_day = 4
-                }
-                if (day === 'saturday')
-                {
-                    markup_day = 5
-                }
-                if (day === 'sunday')
-                {
-                    markup_day = 6
-                }
-                num_of_days = 8 - current_day + 7 * i + markup_day
+                num_of_days = 8 - current_day + 7 * i + week_index[day]
 
                 const ticket_datetime = new Date(new Date().getTime() + num_of_days*24*60*60*1000)
-                const ticket = Ticket.create({table_id:board._id, user_id: null , datetime: ticket_datetime, duration: markup.duration,is_outdated: false, enabled: false, confirmed: false})
-                // __
-                console.log('2')
+                
+                const ticket = await Ticket.create({table_id:board._id, user_id: null , datetime: ticket_datetime, duration: markup.duration,is_outdated: false, enabled: false, confirmed: false})
+                console.log(`${i}: ${day}`)
+                
+                board.tickets[i][day].push(ticket._id)
 
                 add_min(duration)
-                console.log(ticket_time)
             } while(ticket_time < close_time.getTime())
         }
-            
+           
         }
-        res.status(200).json({
-            message: "Board created successfully",
-            board
-        })
+        board.save((error) => {
+            //Monogodb error checker
+            if (error) {
+              return res
+                .status(200)
+                .json({ message: "An error occurred", error: error.message });
+            }
+            res.status(201).json({ message: "Created successfully", board});
+        });
+
+        // res.status(200).json({
+        //     message: "Board created successfully",
+        //     board
+        // })
 
     } catch (error) {
         res.status(400).json({
@@ -105,7 +159,6 @@ const create  = asyncHandler(async (req, res) => {
         })
 
     }
-
 })
 
 const deleteBoard = async (req, res) => {
