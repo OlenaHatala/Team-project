107d32d94dc9d29dca133708a7cacdb862ad9309
ayatsuliak <andrii.yatsuliak@gmail.com>
2023-02-16 21:02:42 +0200

    fixed env file

--- back-end/config/gitignoredConstants.js
+++
@@ -1,8 +0,0 @@
-require("dotenv").config();
-
-module.exports = Object.freeze({
-    USER: process.env.USER,
-    PASSWORD: process.env.PASSWORD,
-    ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET, 
-    REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET
-});
