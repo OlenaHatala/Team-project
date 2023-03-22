537986e8fed9bbe1f293d8d200780fe64dcf0095
ayatsuliak <andrii.yatsuliak@gmail.com>
2023-03-22 15:00:03 +0200

    change create in ticketController

--- back-end/controllers/ticketController.js
+++ back-end/controllers/ticketController.js
@@ -77,41 +77,6 @@ const create = asyncHandler(async (req, res) => {
   }
 });
 
-// const create = asyncHandler(async (req, res) => {
-//   const { table_id, user_id, datetime, duration, is_outdated, enabled, confirmed } = req.body; 
-//   if (!table_id || !datetime || !duration || !is_outdated || !enabled || !confirmed) {
-//       return res.status(400).json({ message: 'All fields are required' })
-//   }
-//   const new_table_id = new ObjectId(table_id);
-//   const new_user_id = user_id === "" ? null : new ObjectId(user_id); // convert empty string to null
-
-//   try {   
-//       const existingTicket = await Ticket.findOne({ datetime });
-//       if (existingTicket) {
-//         return res.status(400).json({ message: 'A ticket already exists with this datetime' });
-//       }
-//       const newTicket = new Ticket({
-//         table_id: new_table_id,
-//         user_id: new_user_id,         
-//         datetime, 
-//         duration, 
-//         is_outdated, 
-//         enabled, 
-//         confirmed
-//       });
-//       const savedTicket = await newTicket.save();
-//       res.status(200).json({
-//         message: "Ticket created successfully",
-//         ticket: savedTicket,
-//       });
-//     } catch (error) {
-//       res.status(400).json({
-//         message: "Ticket was not created",
-//         error: error.message,
-//       });
-//     }
-// });
-
 const read = asyncHandler(async (req, res) => {
   const { id } = req.body
   try {
