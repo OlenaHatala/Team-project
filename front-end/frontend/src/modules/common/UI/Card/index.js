c271bedee20129e7244aab25718022b077e1b67d
Maria Padalka <ms.padalka@gmail.com>
2023-04-13 02:27:15 +0300

    fix styles

--- front-end/frontend/src/modules/common/UI/Card/index.js
+++ front-end/frontend/src/modules/common/UI/Card/index.js
@@ -3,8 +3,9 @@ import classes from "./Card.module.css";
 const Card = (props) => {
   return (
     <div
+      style={ props.style }
       className={`${classes.card} ${
-        props.color === 'white' ? classes.white : ""
+        props.color === "white" ? classes.white : ""
       }`}
     >
       {props.children}
--- front-end/frontend/src/modules/tickets/components/TakenTicket/index.js
+++ front-end/frontend/src/modules/tickets/components/TakenTicket/index.js
@@ -1,35 +1,39 @@
 import classes from "./TakenTicket.module.css";
 import { Icon } from "react-icons-kit";
-import {calendar} from 'react-icons-kit/icomoon/calendar';
-import {clock} from 'react-icons-kit/icomoon/clock';
-
+import { calendar } from "react-icons-kit/icomoon/calendar";
+import { clock } from "react-icons-kit/icomoon/clock";
 
 const TakenTicket = (props) => {
-  console.log(props)
-  console.log(props.status)
-    return (
-        <div className={classes.ticket}>
-        <div className={`${classes["ticket-header"]} ${classes[`${props.status}`]}`}>
-          {props.status}
+  console.log(props);
+  console.log(props.status);
+  return (
+    <div className={classes.ticket}>
+      <div
+        className={`${classes["ticket-header"]} ${classes[`${props.status}`]}`}
+      >
+        {props.status}
+      </div>
+      <div className={classes["ticket-title"]}>
+        {props.boardName.slice(0, 40)}
+      </div>
+      <hr />
+      <h5 className={classes["ticket-details-header"]}>Date and time:</h5>
+      <div className={classes["ticket-details"]}>
+        <div className={classes["ticket-date"]}>
+          <span>
+            <Icon icon={calendar} size={14} />
+          </span>
+          {props.date.toDateString()}
         </div>
-        <div className={classes["ticket-title"]}>
-          {props.boardName.slice(0, 40)}
-        </div>
-        <hr />
-        <h5 className={classes["ticket-details-header"]}>Date and time:</h5>
-        <div className={classes["ticket-details"]}>
-          <div className={classes["ticket-date"]}>
-          <span><Icon icon={calendar} size={14} /></span>
-            {props.date.toDateString()}
-          </div>
-          <div className={classes["ticket-time"]}>
-          <span><Icon icon={clock} size={14}/></span>
-            {props.time}
-          </div>
+        <div className={classes["ticket-time"]}>
+          <span>
+            <Icon icon={clock} size={14} />
+          </span>
+          {props.time}
         </div>
       </div>
-    );
-  };
-
+    </div>
+  );
+};
 
 export default TakenTicket;
