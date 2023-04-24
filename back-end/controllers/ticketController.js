ead0abc3ccc33ac9437983b0da2267387a02f21e
Maria Padalka <ms.padalka@gmail.com>
2023-04-24 23:13:12 +0300

    change timezoneOFFset

--- back-end/controllers/ticketController.js
+++ back-end/controllers/ticketController.js
@@ -25,7 +25,8 @@ async function check_if_outdated(id){
     }
 
     let currentDate = new Date(); // today
-    const timezoneOffset = currentDate.getTimezoneOffset(); // Get the difference in minutes between the local time zone and UTC time
+    const timezoneOffset = 60
+    // const timezoneOffset = currentDate.getTimezoneOffset(); // Get the difference in minutes between the local time zone and UTC time
     var new_current_time = new Date(currentDate.getTime() - (timezoneOffset * 60 * 1000)); // Adjust the time by the offset
     let diffTime = ticket.datetime.getTime() - new_current_time.getTime();
     if(diffTime < 0)
